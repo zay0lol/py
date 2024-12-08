4c99dadcd0a10da2b57e3d317303b2d225bd7367
zay0lol <zynahisthebest2012@gmail.com>
2024-12-08 08:50:57 -0800

    python 1

---
+++ main.py
@@ -0,0 +1,10 @@
+name="Zynah"
+age= 12
+likes="coding and sleeping"
+print("Hi my name is",name,"and my age is",age,"and i like ",likes )
+name2="Huda"
+age2=12
+likes2="resting and watching shows"
+print("Hi my name is",name2,"and my age is",age2,"and my likings are",likes2)
+import keyword
+print(keyword.kwlist)
